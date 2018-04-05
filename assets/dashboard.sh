fe743c688c77570e17e9ebd58a17f69801040cd5
Secrect Nguyen <quoclong.hacker1412@gmail.com>
2018-04-05 18:18:10 +0700

    fix

--- assets/dashboard.sh
+++ assets/dashboard.sh
@@ -3,10 +3,5 @@
 for CODE_NAME  in $DEAMON_RUN  ; do
 	source /usr/local/ezlite/cryptos/${CODE_NAME}/spec.ezs
 	BALANCE=$($MN_CLI --datadir=/root/masternode/${CRYPTO_NAME}/ --conf=/root/masternode/${CRYPTO_NAME}/${CRYPTO_NAME}.conf ${BALANCE_MN})
-	DATE=$(date +%d-%m-%y)
-	echo -e "$DATE ; $BALANCE" >> /root/ezlite/report/${CODE_NAME}.csv
+
 done
-
-
-
-
