08d7e06010461ea65bff6549d38bca6239e12092
Secrect Nguyen <quoclong.hacker1412@gmail.com>
2018-04-08 16:12:48 +0700

    fix

--- ezlite.sh
+++ ezlite.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# DIR 
-EZ_DIR=/usr/local/ezlite
-EZ_DATA=/root/ezlite
-MN_DIR=/root/masternode
 # SOURCE 
 mn_address=$(ip route get 1 | awk '{print $NF;exit}');
 rd_passwd=$(date +%s | sha256sum | base64 | head -c 32 ;);
@@ -12,12 +8,15 @@ wl_passwd=$(date +%s | sha256sum | base64 | head -c 16 ;);
 # Trap CTRL+C, CTRL+Z and quit singles
 #trap '' SIGINT SIGQUIT SIGTSTP
 # LOGIC
-	while true
-	do	
-		tput clear
-		source $EZ_DIR/assets/banner.ezs
-		source $EZ_DIR/assets/menu.ezs
-		show_main_banner
-		display_main_menu
-		action_main_menu
-	done
+
+while true
+do	
+	source /usr/local/ezlite/assets/banner.ezs
+	source /usr/local/ezlite/assets/menu.ezs
+	tput clear
+	tput smcup
+	show_main_banner 
+	display_main_menu
+	action_main_menu
+done
+
