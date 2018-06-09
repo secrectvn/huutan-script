c3f2480d797bfcd85ff9231e5f712cb792bf0855
Unknown <quoclong.hacker1412@gmail.com>
2018-06-09 14:01:26 +0700

    update v2

--- ezmn.sh
+++ ezmn.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-source /usr/local/ezmn/source.ezs ;
+source /usr/local/ezmn/assets/source.ezs ;
 
 ################
 # Trap CTRL+C, CTRL+Z and quit singles
 #trap '' SIGINT SIGQUIT SIGTSTP
 # LOGIC
-while true
-do	
-	s_main_menu	
+
+main_menu(){
+	s_main_menu
 	display_main_menu
 	action_main_menu
+
+}
+
+while true
+do	
+	tput clear
+	main_menu
+
 done
+pause
 
